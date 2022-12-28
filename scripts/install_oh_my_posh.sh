9302e1a647e4d8429d9e31c2f4b5f5d3a4434958
alicarpio <agcarpio@espol.edu.ec>
2022-12-28 15:51:01 -0500

    Update install_oh_my_posh script

--- scripts/install_oh_my_posh.sh
+++ scripts/install_oh_my_posh.sh
@@ -3,16 +3,20 @@
 # Do nothing if oh-my-posh is already installed
 if command -v oh-my-posh >/dev/null; then
 	echo 'oh-my-posh is already installed'
-	exit 0
+else
+	# Install oh-my-posh
+	sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+	sudo chmod +x /usr/local/bin/oh-my-posh
 fi
 
-# Install oh-my-posh
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-sudo chmod +x /usr/local/bin/oh-my-posh
+if [ -d ~/.poshthemes ]; then
+	echo 'poshthemes are already installed'
+else
+	# Install themes
+	mkdir ~/.poshthemes
+	wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+	unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+	chmod u+rw ~/.poshthemes/*.omp.*
+	rm ~/.poshthemes/themes.zip
+fi
 
-# Install themes
-mkdir ~/.poshthemes
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-chmod u+rw ~/.poshthemes/*.omp.*
-rm ~/.poshthemes/themes.zip
