a6221ae1d21c1763dbbae8322499b61903f4cd3a
Aline <aline@pop-os.localdomain>
2022-12-28 15:45:40 -0500

    add script to install oh-my-posh

---
+++ scripts/install_oh_my_posh.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Do nothing if oh-my-posh is already installed
+if command -v oh-my-posh >/dev/null; then
+	echo 'oh-my-posh is already installed'
+	exit 0
+fi
+
+# Install oh-my-posh
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+# Install themes
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+rm ~/.poshthemes/themes.zip
